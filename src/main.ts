66d61e9acfce944e4741cac04942a81d795b322d
adriandcoding <theamr91@gmail.com>
2024-05-10 11:33:28 +0200

    añadiendo modulo motor

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { partida } from "./modelo";
+import {
+  obtenerNumeroAleatorio,
+  obtenerNumeroCarta,
+  obtenerValorCarta,
+  sumarPuntos,
+asignarNuevosPuntos,obtenerUrlImagen,valorar} from "./motor";
 
 //función para mostrar la puntuacion
 const mostrarPuntuacion = () => {
@@ -10,25 +16,8 @@ const mostrarPuntuacion = () => {
 };
 document.addEventListener("DOMContentLoaded", mostrarPuntuacion);
 
-//función para generar carta aleatoria
-const obtenerNumeroAleatorio = (): number => {
-  return Math.floor(Math.random() * 10) + 1;
-};
-const obtenerNumeroCarta = (numeroAleatorio: number): number => {
-  if (numeroAleatorio > 7) {
-    return numeroAleatorio + 2;
-  }
 
-  return numeroAleatorio;
-};
-//función para obtener el valor de la carta
-const obtenerValorCarta = (carta: number): number => {
-  if (carta > 7) {
-    return 0.5;
-  }
 
-  return carta;
-};
 // para pintar el url de cada carta
 const pintarUrlImagen = (urlCarta: string): void => {
   const cartaMostrada = document.querySelector(".cardfront");
@@ -37,13 +26,7 @@ const pintarUrlImagen = (urlCarta: string): void => {
     cartaMostrada.src = urlCarta;
   }
 };
-//generar puntuación
-const sumarPuntos = (puntos: number) => {
-  return partida.puntosTotales + puntos;
-};
-const asignarNuevosPuntos = (nuevosPuntos: number) => {
-  partida.puntosTotales = nuevosPuntos;
-};
+
 //funcionalidad para el botón de game over
 const gameOver = (): void => {
   alert("¡HAS PERDIDO!\u{1F600}");
@@ -76,58 +59,7 @@ if (pedirCarta && pedirCarta instanceof HTMLButtonElement) {
   pedirCarta.addEventListener("click", pideCarta);
 }
 
-//capturando las url de las imagenes
-const obtenerUrlImagen = (carta: number): string => {
-  let imagen;
-  // Mapeando el valor
-  switch (carta) {
-    case 1:
-      imagen =
-        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg";
-      break;
-    case 2:
-      imagen =
-        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg";
-      break;
-    case 3:
-      imagen =
-        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg";
-      break;
-    case 4:
-      imagen =
-        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg";
-      break;
-    case 5:
-      imagen =
-        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg";
-      break;
-    case 6:
-      imagen =
-        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg";
-      break;
-    case 7:
-      imagen =
-        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg";
 
-      break;
-    case 10:
-      imagen =
-        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg";
-      break;
-    case 11:
-      imagen =
-        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg";
-      break;
-    case 12:
-      imagen =
-        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg";
-      break;
-    default:
-      imagen =
-        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
-  }
-  return imagen;
-};
 // nueva partida
 const nuevaPartida = (): void => {
   partida.puntosTotales = 0;
@@ -147,24 +79,7 @@ if (botonNuevaPartida && botonNuevaPartida instanceof HTMLButtonElement) {
   botonNuevaPartida.addEventListener("click", nuevaPartida);
 }
 
-//valorar la opción al plantarse
-const valorar = (): string => {
-  let mensaje: string = "";
-  if (partida.puntosTotales < 4) {
-    mensaje = "Has sido muy conservador";
-  }
-  if (partida.puntosTotales === 5) {
-    mensaje = "Te ha entrado el canguelo eh?";
-  }
-  if (partida.puntosTotales === 6 || partida.puntosTotales === 7) {
-    mensaje = "Casi casi...";
-  }
-  if (partida.puntosTotales === partida.numeroGanador) {
-    mensaje = "¡ Lo has clavado! ¡Enhorabuena!";
-  }
 
-  return mensaje;
-};
 //funcionalidad de plantarse
 const plantarse = (): void => {
   deshabilitarBotonPedirCarta(true);
